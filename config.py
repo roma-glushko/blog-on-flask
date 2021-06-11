0791c7ef072843949443a655b6fa6693fef09139
Roman Glushko <roman.glushko.m@gmail.com>
2021-06-11 17:36:11 +0300

    🏄‍♂️ Inited the blog app: - added login page - added index page - added login form - added SQLite client and DB migration framework - inited User and Post models

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+basedir: str = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'test-secret-token-042'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
